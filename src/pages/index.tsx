fc0fdbc64201ebd8bf96b5f2fefad436a7d4d909
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-04-24 15:20:15 -0300

    feat: add posts router, remove example router n render posts into index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery(); 
 
   return (
     <>
@@ -22,7 +22,11 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post => (
+          <div key={post.id}>{post.content}</div>
+          )))}
+        </div>
       </main>
     </>
   );
