30042732a97e2c1dc386b46665a1f33fc5e81a6b
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-04-24 17:50:27 -0300

    feat: add CreatePostWizard component n adjust posts return

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,33 @@
+import { User } from "@clerk/nextjs/dist/api";
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.username,
+    profileImageUrl: user.profileImageUrl
+  }
+}
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100
+    });
+
+    const users = (
+        await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId)
+    }));
   }),
 });
