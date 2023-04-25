19c69cb53d3c5bddb37c92fe57da45e05af3f463
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-04-25 14:13:19 -0300

    style: font size post content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,7 +50,7 @@ const PostView = ({ post, author }: PostWithUser) => {
           <span>{`@${author.username}`}</span>
           <span className="font-thin">{` - ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
   </div>
   )
