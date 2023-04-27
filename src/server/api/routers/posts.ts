d53922fbd0b33c51f7eacf95afd69a6734c92302
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-04-27 17:48:09 -0300

    feat: add profile router n change pages head titles

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,20 +1,11 @@
-import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
-
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl
-  }
-}
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 10 requests per 10 seconds
 const ratelimit = new Ratelimit({
