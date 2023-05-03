866a67048e0f809cd0fde534839d9e8429b154a0
Leonardo Silva <leonardohsilva.dev@gmail.com>
2023-05-03 09:29:32 -0300

    feat: buildin profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -6,13 +6,12 @@ import { prisma } from '~/server/db';
 import superjson from "superjson";
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import { PageLayout } from "~/components";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{username: string}> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({ username });
 
   if (!data) return <div>404</div>
-
-  console.log(username);
 
   return (
     <>
@@ -20,7 +19,18 @@ const ProfilePage: NextPage<{username: string}> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ''}`}</div>
+        <div className="border-b border-slate-400 w-full"></div>
       </PageLayout>
     </>
   );
